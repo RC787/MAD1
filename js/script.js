476f5ad5af13309b377c56845c880386c927c9e7
RC787 <ravichandrapeyyala26@gmail.com>
2024-06-18 20:40:08 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+# College Website
+
+This project is a responsive website for a college, including the following pages:
+1. Home
+2. About
+3. Products or Services
+4. Contact Us
+
+## How to Use
+
+1. Open `index.html` in your web browser to start.
+2. Navigate through the website using the links in the navigation bar.
+
+## File Structure
+
